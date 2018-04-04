2cf343afa42b424710ab82eec80ab0234beffd3d
GOODEE <GOODEE@DESKTOP-ARIPPJJ>
2018-04-04 11:17:36 +0900

    서블릿 GET & POST 테스트 완료!

---
+++ src/kr/gudi/Main.java
@@ -0,0 +1,23 @@
+package kr.gudi;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPost");
+	}
+	//post -> post를 보내기위한 화면이 필요하다.
+	
+}
